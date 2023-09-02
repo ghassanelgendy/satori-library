82e5a3cae8e5ef2c128b246f413d714f8dc39acc
Ghassan <ghassanelgendyy@gmail.com>
2023-09-02 20:14:12 +0300

    Modal Improvements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
 const book = document.querySelectorAll(".book");
-
-function openModal(el) {}
-book.forEach((book) => {
-	book.addEventListener("click", openModal());
+const addBookModalBtn = document.querySelector(".add");
+const addBookModal = document.querySelector(".modal");
+addBookModalBtn.addEventListener("click", () => {
+	console.log("wee");
+	addBookModal.classList.add("visible");
 });
+const modal = document.querySelector(".modal");
+const except = document.querySelector("form");
+
+document.addEventListener("keydown", (e) => {
+	if (e.key === "Escape") {
+		addBookModal.classList.remove("visible");
+	}
+});
+modal.addEventListener(
+	"click",
+	function () {
+		addBookModal.classList.remove("visible");
+	},
+	false
+);
+except.addEventListener(
+	"click",
+	function (e) {
+		e.stopPropagation();
+	},
+	false
+);
