159114d0ed8695091eb27526267156f73f992c8e
Ghassan <ghassanelgendyy@gmail.com>
2023-09-08 03:09:24 +0300

    Add Comments

--- js/script.js
+++ js/script.js
@@ -2,25 +2,17 @@ const book = document.querySelectorAll(".book");
 const addBookModalBtn = document.querySelector(".add");
 const addBookModal = document.querySelector(".modal");
 const addBookBtn = document.getElementById("addBookBtn");
+const modal = document.querySelector(".modal");
+const except = document.querySelector("form");
+const theLibrary = [];
+
+//Load the "add button" modal
 addBookModalBtn.addEventListener("click", () => {
 	console.log("wee");
 	addBookModal.classList.add("visible");
 });
-const modal = document.querySelector(".modal");
-const except = document.querySelector("form");
-const theLibrary = [];
-function Book() {
-	title;
-	author;
-	pages;
-	isRead = false;
-}
-function addBookToLibrary(title, author, pages, isRead) {}
-document.addEventListener("keydown", (e) => {
-	if (e.key === "Escape") {
-		addBookModal.classList.remove("visible");
-	}
-});
+
+//Close modal by clicking anywhere out of it
 modal.addEventListener(
 	"click",
 	function () {
@@ -35,3 +27,26 @@ except.addEventListener(
 	},
 	false
 );
+
+//Close modal by clicking escape button
+document.addEventListener("keydown", (e) => {
+	if (e.key === "Escape") {
+		addBookModal.classList.remove("visible");
+	}
+});
+
+//Constructor for "Book"
+function Book() {
+	title;
+	author;
+	pages;
+	isRead = false;
+}
+
+//Add a new Book object to theLibrary Array
+function addBookToLibrary(title, author, pages, isRead) {}
+
+//Disable default submitting action for the "submit button"
+addBookBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+});
